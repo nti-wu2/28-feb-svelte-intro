a1fbbb66d5939ec18620d6e05a26bb749837ace6
johan.kivi <johan.kivi@zocom.se>
2022-02-28 17:00:45 +0100

    hello world

--- hello-world/src/App.svelte
+++ hello-world/src/App.svelte
@@ -1,30 +1,22 @@
 <script>
-	export let name;
+let greeting = 'Hello';
+
+function changeText(){
+	greeting = 'good bye!';
+}
+
+let name = '';
+
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<div class="app">
+<h1>{greeting} {name}</h1>
+<input type="text" bind:value={name}>
+<button on:click={changeText}>Change text</button>
+</div>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+h1 {
+	color: red;
+}
 </style>
